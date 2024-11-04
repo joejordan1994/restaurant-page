a18e67a3e93e05d5e57adf0117c3a1ce6b2397d1
spcymshrm <joe.jordan1994@gmail.com>
2024-11-04 15:40:20 -0500

    Updated webpack config and uploaded interior restaurant pic
    - Updated the webpack JSON file to allow for images
    - Found an image of a restaurant and added it to the assets folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,23 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "development", // Change to 'production' when ready
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    assetModuleFilename: "assets/[name][ext]",
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/i, // For CSS files
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // For image files
+        type: "asset/resource",
       },
     ],
   },
